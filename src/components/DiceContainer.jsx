e092b66a281cff31abb9b5ae02541f3a6d74914b
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-03-06 00:30:34 +0100

    Add dice container component and styling to it

---
+++ src/components/DiceContainer.jsx
@@ -0,0 +1,7 @@
+import StyledDiceContainer from "./styles/DiceContainer.styled";
+
+const DiceContainer = ({ children }) => {
+  return <StyledDiceContainer>{children}</StyledDiceContainer>;
+};
+
+export default DiceContainer;
---
+++ src/components/styles/DiceContainer.styled.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const StyledDiceContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 1.3em;
+  width: 90%;
+`;
+
+export default StyledDiceContainer;
