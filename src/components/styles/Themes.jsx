e3dca8c5bb87578f0dccab6bf3e81bd7fa17719c
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-03-09 23:29:44 +0100

    Add styled components

---
+++ src/components/styles/Themes.jsx
@@ -0,0 +1,11 @@
+const theme = {
+  colors: {
+    background: "#0B2434",
+    main: "#F5F5F5",
+    button: "#5035FF",
+    dice: "#FFFFFF",
+    clickedDice: "#59E391",
+  },
+};
+
+export default theme;
