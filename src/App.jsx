27db5b9fd9ff48082a5c436d4bbb7cf4784157dc
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-03-10 00:49:28 +0100

    Add initial App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,19 @@
+import Theme from "./components/styles/Theme";
+import Main from "./components/Main";
+import DiceContainer from "./components/diceContainer";
+import GlobalStyle from "./components/styles/Global";
+
 function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Theme>
+        <Main>
+          <DiceContainer></DiceContainer>
+        </Main>
+      </Theme>
+    </>
+  );
 }
 
-export default App
+export default App;
