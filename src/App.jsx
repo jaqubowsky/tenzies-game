923731a6df06d433fd4e85b8faa036139223ed4a
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-03-10 00:50:02 +0100

    Add state and mapping to generate random dices

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
+import { useState } from "React";
 import Theme from "./components/styles/Theme";
 import Main from "./components/Main";
+import Dice from "./components/Dice";
 import DiceContainer from "./components/diceContainer";
 import GlobalStyle from "./components/styles/Global";
 
 function App() {
+  const [diceArray, setDiceArray] = useState(getAllNewDice());
+
+  function getAllNewDice() {
+    return Array.from({ length: 10 }, () => Math.floor(Math.random() * 6) + 1);
+  }
+
+  const diceElements = diceArray.map((dice) => (
+    <Dice key={Math.random()} value={dice} />
+  ));
+
   return (
     <>
       <GlobalStyle />
       <Theme>
         <Main>
-          <DiceContainer></DiceContainer>
+          <DiceContainer>{diceElements}</DiceContainer>
         </Main>
       </Theme>
     </>
