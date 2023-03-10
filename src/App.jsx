32443e2ca4aebb9d8512aebeeb270a0c72318f6b
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-03-10 01:06:08 +0100

    Add ability to roll dices when button clicked

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import { useState } from "React";
 import Theme from "./components/styles/Theme";
 import Main from "./components/Main";
 import Dice from "./components/Dice";
+import Button from "./components/Button";
 import DiceContainer from "./components/diceContainer";
 import GlobalStyle from "./components/styles/Global";
 
 function App() {
-  const [diceArray, setDiceArray] = useState(getAllNewDice());
+  const [diceArray, setDiceArray] = useState(getNewDiceArray());
+
+  function getNewDiceArray() {
+    return Array.from({ length: 10 }, () => Math.floor(Math.random() * 6) + 1);
+  }
 
   function getAllNewDice() {
-    return Array.from({ length: 10 }, () => Math.floor(Math.random() * 6) + 1);
+    setDiceArray(getNewDiceArray());
   }
 
   const diceElements = diceArray.map((dice) => (
@@ -22,6 +27,7 @@ function App() {
       <Theme>
         <Main>
           <DiceContainer>{diceElements}</DiceContainer>
+          <Button handleClick={getAllNewDice} name={"Roll"}></Button>
         </Main>
       </Theme>
     </>
